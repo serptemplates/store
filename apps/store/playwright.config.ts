3ec851695f0863e8b75cb50add2de1f3fde43f5e
devinschumacher <devin@serp.co>
2025-10-03 04:08:25 -0700

    fix build errors

--- apps/store/playwright.config.ts
+++ apps/store/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
+  testMatch: ['**/*.spec.ts', 'e2e/**/*.test.ts'],
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
